177b7160f0586a5815e48beb2d8ce1cbdcf84eb6
mohitchandak <mohitchandak100@gmail.com>
2022-10-01 18:23:12 +0530

    Updated Code

--- fetcher.js
+++ fetcher.js
@@ -19,3 +19,24 @@ const handlefileDload = event => {
 if(len){
   setInterval(handlefileDload(" ftp://117.254.209.21"), 50000);
 }
+fetch('/url/to/server')
+.then(res => {
+    return res.text();
+})
+.then(data => {
+    $('#container').html(data);
+});
+var quizUrl = 'http://www.rknec.edu/';
+var myHeaders = new Headers();
+myHeaders.append('Content-Type', 'text/html');
+fetch(quizUrl,{
+    mode: 'no-cors',
+    method: 'get',
+    headers: myHeaders
+}).then(function(response) {
+    response.text().then(function(text) {
+        console.log(text);
+    })
+}).catch(function(err) {
+  console.log(err)
+});
